8edd548ad404b8bea371a4419d229221fc1ea911
Michell2022 <sistemas.angel04@gmail.com>
2022-12-29 18:58:19 -0500

    funcion dado

--- src/app/elementos/dado/dado.component.ts
+++ src/app/elementos/dado/dado.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./dado.component.css']
 })
 export class DadoComponent {
-  numero:String='0';
+
+  numero= 0;
+  resultado=0;
+  tirarDados(){
+    
+    this.resultado = this.numero = Math.floor((Math.random() * 6) + 1);
+
+    console.log(this.numero)
+
+  }
 }
